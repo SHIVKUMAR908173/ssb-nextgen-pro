c205504331e2e0aee67095088149b9477db1b025
Shivkumar <shivkumar@ssbnextgen.pro>
2026-08-12 18:24:27 +0530

    chore: add custom module declarations for @modelcontextprotocol/sdk

---
+++ backend-core/src/custom.d.ts
@@ -0,0 +1,9 @@
+declare module '@modelcontextprotocol/sdk/*' {
+  const _: any;
+  export default _;
+}
+
+declare module '@modelcontextprotocol/sdk/client/index.js' {
+  const _: any;
+  export default _;
+}
